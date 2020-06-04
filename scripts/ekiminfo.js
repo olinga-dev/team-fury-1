8b5738e8b04a080cfdfb10bf92a181d05e18fa49
alabanibaba <micheal.adeniyi@gmail.com>
2020-06-04 16:09:32 -0700

    Update ekiminfo.js
    Hello I have changed the format of the script.

--- scripts/ekiminfo.js
+++ scripts/ekiminfo.js
@@ -1,7 +1,7 @@
-var name = "Micheal Ayeni"
-var ID = "HNG-05908"
+var fullname = "Micheal Ayeni"
+var hng_id = "HNG-05908"
 var language = "Javascript"
 var email = "ekiminfomedia@gmail.com"
-let output = `Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+let output = `Hello World, this is ${fullname} with HNGi7 ID ${hng_id} using ${language} for stage 2 task`;
 console.log(output)
 
