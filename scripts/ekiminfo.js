a39af9ef242d8039f7eba0e80ef87e260dbbf2a4
alabanibaba <micheal.adeniyi@gmail.com>
2020-06-04 23:52:16 -0700

    Update ekiminfo.js
    my new script

--- scripts/ekiminfo.js
+++ scripts/ekiminfo.js
@@ -1,7 +1,7 @@
 var fullname = "Micheal Ayeni"
 var hng_id = "HNG-05908"
-var language = "Javascript"
+var language = "javascript"
 var email = "ekiminfomedia@gmail.com"
-let output = `Hello World, this is ${fullname} with HNGi7 ID ${hng_id} using ${language} for stage 2 task`;
+let output = `Hello World, this is ${fullname} with HNGi7 ID ${hng_id} and email ${email} using ${language} for stage 2 task`;
 console.log(output)
 
