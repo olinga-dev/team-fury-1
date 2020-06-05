8ceb9785dafd24a850a3ef5981ec2cb0c7c9f9ba
alabanibaba <micheal.adeniyi@gmail.com>
2020-06-05 00:40:15 -0700

    updated
    new script

--- scripts/ekiminfo.js
+++ scripts/ekiminfo.js
@@ -1,7 +1,12 @@
-var fullname = "Micheal Ayeni"
-var hng_id = "HNG-05908"
-var language = "javascript"
-var email = "ekiminfomedia@gmail.com"
-let output = `Hello World, this is ${fullname} with HNGi7 ID ${hng_id} and email ${email} using ${language} for stage 2 task`;
+//my data
+let fullname = "Micheal Ayeni"
+let hng_id = "HNG-05908"
+let language = "javascript"
+let email = "ekiminfomedia@gmail.com"
+
+//my message
+const output = `Hello World, this is ${fullname} with HNGi7 ID ${hng_id} and email ${email} using ${language} for stage 2 task`;
+
+//my output
 console.log(output)
 
